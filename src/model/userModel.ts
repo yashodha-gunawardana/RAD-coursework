4651505f42aca27bc4954ade179a97afdefe10d0
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-23 01:01:21 +0530

    define the structure of User documnet in database

--- src/model/userModel.ts
+++ src/model/userModel.ts
@@ -1,4 +1,4 @@
-import { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 
 export enum Role {
     ADMIN = "ADMIN",
@@ -10,4 +10,15 @@ export enum Status {
     PENDING = "PENDING",
     APPROVED = "APPROVED",
     REJECTED = "REJECTED"
+}
+
+export interface IUser extends Document {
+    _id: mongoose.Types.ObjectId
+    fullname: string
+    email: string
+    password: string
+    address?: string
+    phone?: string
+    roles: Role[]  // array of roles assigned to user
+    approved: Status
 }
