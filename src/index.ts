f84f3232855528f15c23b7ba5037fe236b1f00fd
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-22 23:59:41 +0530

    set the port from .env file

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,10 @@ import mongoose from "mongoose";
 
 dotenv.config();
 
+const SERVER_PORT = process.env.SERVER_PORT
+
+const app = express();
+
+// json data parse in incoming requests
+app.use(express.json());
+
