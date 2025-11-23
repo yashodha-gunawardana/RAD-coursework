51a9f8dc27844a065f819844022bfc4bbf370354
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-23 15:25:20 +0530

    middleware to allow access only for specific roles

--- src/middleware/roleMiddleware.ts
+++ src/middleware/roleMiddleware.ts
@@ -1,3 +1,13 @@
 import { Response, NextFunction } from "express";
 import { Role } from "../model/userModel";
 import { AuthRequest } from "./authMiddleware";
+
+
+// middleware to allow access only for specific roles
+export const requiredRole = (roles: Role[]) => {
+    return (req: AuthRequest, res: Response, next: NextFunction) => {
+
+    }    
+    
+    
+}
