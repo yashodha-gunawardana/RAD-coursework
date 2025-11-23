86dc41a276aa30d2f78c71d5713da767404d8227
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-23 11:18:34 +0530

    get data sent from frontend

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,5 +3,9 @@ import { User } from "../model/userModel";
 
 
 export const registerUser = async (req: Request, res: Response) => {
-    
+    try {
+        const { fullname, email, password, address, phone } = req.body;
+    } catch (err) {
+
+    }
 }
