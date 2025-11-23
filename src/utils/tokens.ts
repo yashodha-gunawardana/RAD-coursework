3ca36c2c092788bc5007af837c1c389970a02fbc
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-23 16:54:32 +0530

    sub (subject) is a standard JWT claim, storing the user's unique ID

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -10,7 +10,10 @@ const JWT_SECRET = process.env.JWT_SECRET as string
 // generate a JSON web token for a given user
 export const signAccessToken = (user: IUser): string => {
     return jwt.sign(
-        
+        {
+            // `sub` (subject) is a standard JWT claim, storing the user's unique ID
+            sub: user._id.toString(),
+        }
     )
 
 }
