f2916d1767f9d450dedd1700761ccd2c65d05e1e
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-27 00:03:39 +0530

    create new event object based on request data

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -1,9 +1,24 @@
+import { Request, Response } from "express";
+import Event, { IEvent, Status } from "../model/eventModel";
 
 
 export const createEvent = async (req: Request, res: Response) => {
     try {
         const { userId, title, type, date, time, location, description, image } = req.body
 
+        const newEvent = new Event({
+            userId,
+            title,
+            type,
+            date,
+            time,
+            location,
+            description,
+            image,
+            status: Status.PLANNING,
+        })
+
+       
     } catch (err) {
 
     }
