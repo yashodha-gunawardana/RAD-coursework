2f91fc5df414b4bb44926612240df90c08d42401
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-26 23:56:18 +0530

    extract event data sent from client request body

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -1,5 +1,10 @@
 
 
 export const createEvent = async (req: Request, res: Response) => {
-    
+    try {
+        const { userId, title, type, date, time, location, description, image } = req.body
+
+    } catch (err) {
+
+    }
 }
