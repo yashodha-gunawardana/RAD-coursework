fd03543b08532669cbb1b056f3133d2c26328020
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-27 01:09:41 +0530

    these routes handle all CRUD operations for event

--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -2,3 +2,9 @@ import { Router } from "express";
 import { createEvent, getEvents, getEventById, updateEvent, deleteEvent } from "../controllers/eventController";
 
 const router = Router();
+
+router.post("/", createEvent)
+router.get("/", getEvents)
+router.get("/:id", getEventById)
+router.put("/:id", updateEvent)
+router.delete("/:id", deleteEvent)
