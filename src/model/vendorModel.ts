8edf3a143ccdd541c1b70ce1f6d7ccbebf27ad28
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-30 15:04:28 +0530

    enum created to controll valid vendor categories

--- src/model/vendorModel.ts
+++ src/model/vendorModel.ts
@@ -1,1 +1,12 @@
 import mongoose, { Document, Schema } from "mongoose";
+
+export enum VendorCategory {
+    PHOTOGRAPY = "PHOTOGRAPY",
+    CATERING = "CATERING",
+    DECORATION = "DECORATION",
+    DJ = "DJ",
+    VENUE = "VENUE",
+    MAKEUP = "MAKEUP",
+    FLORIST = "FLORIST",
+    OTHER = "OTHER"
+}
