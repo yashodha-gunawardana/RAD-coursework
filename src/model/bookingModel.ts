fad83ee8d594864144212949c8d1a7a6bfe1d4b8
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-04 00:22:24 +0530

    take a booking status as a enum

--- src/model/bookingModel.ts
+++ src/model/bookingModel.ts
@@ -1,1 +1,8 @@
 import mongoose, { Document, Schema } from "mongoose";
+
+export enum BookingStatus {
+    PENDING = "PENDING",
+    CONFIRMED = "CONFIRMED",
+    CANCELLED = "CANCELLED",
+    COMPLETED = "COMPLETED"
+}
