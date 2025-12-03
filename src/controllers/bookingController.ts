a7bec430ce98b838096c46d4d8e18f8d600cb7b6
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-04 01:07:28 +0530

    handle errors during booking creation and return a 500 response with the error message

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -29,13 +29,16 @@ export const createBooking = async (req: AuthRequest, res: Response) => {
             "vendorTd",
             "name category priceRange image"
         )
-        
+
         return res.status(201).json({
             message: "Your booking successfully..",
             data: populatedBooking
         })
 
-    } catch (err) {
+    } catch (err: any) {
+        return res.status(500).json({
+            message: err?.message
+        })
 
     }
 }
