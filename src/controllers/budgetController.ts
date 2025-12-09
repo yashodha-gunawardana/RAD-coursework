8b60c6d2931cc65200853036fed3facba03938f0
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-09 14:45:15 +0530

    impelement create or update budget function

--- src/controllers/budgetController.ts
+++ src/controllers/budgetController.ts
@@ -2,3 +2,12 @@ import { Request, Response } from "express";
 import Budget, { BudgetStatus } from "../model/budgetModel";
 import Event from "../model/eventModel";
 import { AuthRequest } from "../middleware/authMiddleware";
+
+
+export const createOrUpdateBudget = async (req: AuthRequest, res: Response) => {
+    try {
+
+    } catch (err) {
+        
+    }
+}
