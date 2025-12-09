2075e08d1c1db01fe1536e1ceef0b8b61666ce52
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-09 15:22:37 +0530

    extract request body and user id

--- src/controllers/budgetController.ts
+++ src/controllers/budgetController.ts
@@ -7,7 +7,10 @@ import { AuthRequest } from "../middleware/authMiddleware";
 export const createOrUpdateBudget = async (req: AuthRequest, res: Response) => {
     try {
 
+        const { eventId, selectedItems = [] } = req.body
+        const userId = req.user._id
+
     } catch (err) {
-        
+
     }
 }
